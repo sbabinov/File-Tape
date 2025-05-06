d087b98837ff346e484afc95c2b2b795e175218a
Serafim Babinov <serafimbabinov106@gmail.com>
2025-05-06 21:12:19 +0300

    feat: add config reading

--- main.cpp
+++ main.cpp
@@ -16,13 +16,27 @@ int main(int argc, char* argv[])
     ramLimit = std::stoull(argv[3]);
   }
 
+  tapes::Config config;
+  std::ifstream confFile("../tape.config");
+  if (!confFile)
+  {
+    std::cerr << "Cannot open config file" << '\n';
+    return 1;
+  }
+  confFile >> config;
+  if (confFile.fail())
+  {
+    std::cerr << "Invalid config file" << '\n';
+    return 1;
+  }
+
   converter::FileConverter inConverter(argv[1]);
   converter::FileConverter outConverter(argv[2]);
   size_t inTapeSize = inConverter.toBinary();
   size_t outTapeSize = outConverter.toBinary();
 
-  auto inTape = std::make_shared< tapes::FileTape >(argv[1], inTapeSize, tapes::Config());
-  auto outTape = std::make_shared< tapes::FileTape >(argv[2], outTapeSize, tapes::Config());
+  auto inTape = std::make_shared< tapes::FileTape >(argv[1], inTapeSize, config);
+  auto outTape = std::make_shared< tapes::FileTape >(argv[2], outTapeSize, config);
 
   tapes::Sorter sorter(inTape, outTape, 7);
   sorter();

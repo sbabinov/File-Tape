d86892f985b131041a2ba0785b59fd303887836a
Serafim Babinov <serafimbabinov106@gmail.com>
2025-05-06 22:21:12 +0300

    feat: add unit tests

---
+++ tests/test_sorting.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <memory>
+#include <cstdio>
+#include "gtest/gtest.h"
+#include "../src/tapes.hpp"
+#include "../src/converter.hpp"
+
+class TapeSortTest: public testing::TestWithParam< size_t >
+{
+protected:
+    void SetUp() override
+    {
+      testNum = GetParam();
+      inFilename = "../tests/tapes/inp" + std::to_string(testNum) + ".tp";
+      outFilename = "../tests/tapes/out" + std::to_string(testNum) + ".tp";
+      expectedFilename = "../tests/expected_output/out" + std::to_string(testNum) + ".tp";
+    }
+    size_t testNum;
+    std::string inFilename;
+    std::string outFilename;
+    std::string expectedFilename;
+};
+
+TEST_P(TapeSortTest, SortsCorrectly)
+{
+  using iter = std::istream_iterator< int >;
+
+  converter::FileConverter inConverter(inFilename);
+  size_t tapeSize = inConverter.toBinary();
+
+  auto inTape = std::make_shared< tapes::FileTape >(inFilename, tapeSize, tapes::Config{});
+  auto outTape = std::make_shared< tapes::FileTape >(outFilename, tapeSize, tapes::Config{});
+  tapes::Sorter sorter(inTape, outTape, (tapeSize / 3 > 0) ? (tapeSize / 3) : 1);
+  sorter();
+
+  converter::FileConverter(outFilename).toText();
+  inConverter.toText();
+
+  std::ifstream out(outFilename);
+  std::ifstream expectedOut(expectedFilename);
+
+  EXPECT_TRUE(std::equal(iter(out), iter(), iter(expectedOut)));
+
+  std::remove(outFilename.c_str());
+}
+
+INSTANTIATE_TEST_SUITE_P(TapeTests, TapeSortTest, testing::Values(1, 2, 3, 4, 5, 6, 7, 8, 9));
